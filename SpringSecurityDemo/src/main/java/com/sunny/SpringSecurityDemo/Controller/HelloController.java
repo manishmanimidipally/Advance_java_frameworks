838307910dfd2f99119331be8a97bd684b9630fe
manishmamidipally <mamidipallymanish6@gmail.com>
2026-09-12 23:03:58 +0530

    Spring Security Demo

--- SpringSecurityDemo/src/main/java/com/sunny/SpringSecurityDemo/Controller/HelloController.java
+++ SpringSecurityDemo/src/main/java/com/sunny/SpringSecurityDemo/Controller/HelloController.java
@@ -3,12 +3,15 @@ package com.sunny.SpringSecurityDemo.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController()
 
 public class HelloController {
 	
 	@GetMapping("/")
-	public String greet() {
-		return "Hello World ";
+	public String greet(HttpServletRequest req) {
+		return "Hello World \n"+req.getSession().getId();
+		
 	}
 }
